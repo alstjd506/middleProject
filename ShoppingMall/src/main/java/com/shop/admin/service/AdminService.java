62d78353aa02fa6d26772ebe26731ce76f43328a
ass6728 <ass6728@naver.com>
2024-05-23 00:19:53 +0900

    죄송..

--- ShoppingMall/src/main/java/com/shop/admin/service/AdminService.java
+++ ShoppingMall/src/main/java/com/shop/admin/service/AdminService.java
@@ -2,10 +2,13 @@ package com.shop.admin.service;
 
 import java.util.List;
 
+import com.shop.vo.BoardVO;
 import com.shop.vo.MemberVO;
 import com.shop.vo.ProductVO;
 
 public interface AdminService {
 	List<ProductVO> adminProductList();
 	List<MemberVO> adminMemberList();
+	List<BoardVO> adminBoardList();
+	BoardVO adminBoardInfo(int bno);
 }
