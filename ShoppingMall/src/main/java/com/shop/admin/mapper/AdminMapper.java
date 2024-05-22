c6e069603948bb04f77729479c9700ef3b92fb4c
ass6728 <ass6728@naver.com>
2024-05-23 00:36:41 +0900

    최종커밋.

--- ShoppingMall/src/main/java/com/shop/admin/mapper/AdminMapper.java
+++ ShoppingMall/src/main/java/com/shop/admin/mapper/AdminMapper.java
@@ -14,13 +14,10 @@ import com.shop.vo.ProductVO;
 public interface AdminMapper {
 	List<ProductVO> adminProductList();
 	List<MemberVO> adminMemberList();
-
-
 	List<BoardVO> adminBoardList();
 	BoardVO adminBoardInfo(int bno);
 
+  
 
-	List<BoardVO> adminBoardList();
-	BoardVO adminBoardInfo(int bno);
 
 }
