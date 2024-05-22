eabb201dc4866cec46f86e50ca523919b9e10cab
psjyyb <ass6728@naver.com>
2024-05-22 12:37:41 +0900

    rebase

--- ShoppingMall/src/main/java/com/shop/common/FrontControl.java
+++ ShoppingMall/src/main/java/com/shop/common/FrontControl.java
@@ -39,6 +39,7 @@ public class FrontControl extends HttpServlet {
 		System.out.println("test");
 		System.out.println("test2");
 		System.out.println("test3");
+		System.out.println("test34");
 	}
 
 	// service
