07d328c35bb0294409d5f28c3c791868fd102c6c
psjyyb <ass6728@naver.com>
2024-05-22 12:43:23 +0900

    348

--- ShoppingMall/src/main/java/com/shop/common/FrontControl.java
+++ ShoppingMall/src/main/java/com/shop/common/FrontControl.java
@@ -43,6 +43,7 @@ public class FrontControl extends HttpServlet {
 		System.out.println("test345");
 		System.out.println("test346");
 		System.out.println("test347");
+		System.out.println("test348");
 	}
 
 	// service
