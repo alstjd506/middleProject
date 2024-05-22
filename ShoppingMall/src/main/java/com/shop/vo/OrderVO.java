e6f1c988844d9e280a408d396f943bbef9f8297c
ass6728 <ass6728@naver.com>
2024-05-23 01:46:22 +0900

    박성준 브렌치

---
+++ ShoppingMall/src/main/java/com/shop/vo/OrderVO.java
@@ -0,0 +1,13 @@
+package com.shop.vo;
+
+import java.util.Date;
+
+public class OrderVO {
+	private int orderNo;
+	private String userId;
+	private String orderStatus;
+	private int orderPost;
+	private String orderAddr;
+	private String orderDetailAddr;
+	private Date orderDate;
+}
