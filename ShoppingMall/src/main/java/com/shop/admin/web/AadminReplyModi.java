73378a4d33b40ae7da12afddc18fe11c5446a181
psjyyb <ass6728@naver.com>
2024-05-24 17:24:29 +0900

    박성준

---
+++ ShoppingMall/src/main/java/com/shop/admin/web/AadminReplyModi.java
@@ -0,0 +1,39 @@
+package com.shop.admin.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.shop.admin.service.AdminService;
+import com.shop.admin.service.AdminServiceImpl;
+import com.shop.common.Control;
+import com.shop.vo.ReplyVO;
+
+public class AadminReplyModi implements Control {
+
+	@Override
+	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setContentType("application/json; charset=UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		String bno = req.getParameter("bno");
+		String reply = req.getParameter("reply");
+
+		ReplyVO rvo = new ReplyVO();
+		rvo.setBoardNo(Integer.parseInt(bno));
+		rvo.setReplyContent(reply);
+		AdminService svc = new AdminServiceImpl();
+
+		if (svc.adminReplyModi(rvo)) {
+			resp.getWriter().print("{\"retCode\": \"OK\"}");
+		} else {
+			resp.getWriter().print("{\"retCode\": \"NG\"}");
+		}
+
+	}
+
+}
---
+++ ShoppingMall/src/main/java/com/shop/admin/web/AdminBoadrControl.java
@@ -0,0 +1,39 @@
+package com.shop.admin.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.shop.admin.service.AdminService;
+import com.shop.admin.service.AdminServiceImpl;
+import com.shop.common.Control;
+import com.shop.vo.ReplyVO;
+
+public class AdminBoadrControl implements Control {
+
+	@Override
+	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("컨트롤러 들어옴");
+		String bno = req.getParameter("bodno");
+		String reply = req.getParameter("bodre");
+		
+		System.out.println("게시글번호 : "+bno);
+		System.out.println("답글 : "+reply);
+		ReplyVO rvo = new ReplyVO();
+		
+		rvo.setBoardNo(Integer.parseInt(bno));
+		rvo.setReplyContent(reply);
+		
+		AdminService svc = new AdminServiceImpl();
+		
+		if(svc.adminBoardReply(rvo)&&svc.adminBoardReplyUp(Integer.parseInt(bno))) {
+			resp.sendRedirect("admin.do");
+		}
+		
+
+	}
+
+}
